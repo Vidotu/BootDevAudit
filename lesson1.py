b72b36a515776a3ed39f0ac15b1fc25f0ab2a00f
Grzegorz Bryg <voixmail@gmail.com>
2026-07-22 16:32:03 +0100

    Audit_Setup_Lessons_1_to_3

---
+++ lesson1.py
@@ -0,0 +1,40 @@
+print("Welcome to Fantasy Quest!")
+
+#============
+#LESSON 1 LOG
+#============
+
+# 1. WHAT COMMAND DOES
+# The print() tool sends a line of text string data to your console screen.
+# It requires parenthesis, and text must be wrapped inside quoatation marks.
+
+# 2. UNDER THE HOOD
+# - Phyton turns this line into low-level instructions called bytecode.
+# - The interpreter allocates a block of temporary memory (Ram) to hold text.
+# - It uses sys.stdout to stream the raw data bytes down to the OS kernel.
+#
+# 3. OS AND POWERSHELL INTERACTION
+# - PowerShell starts up python.exe as an isolated sub-process in Windows.
+# - Phyton makes a Win32 system call to WriteConsoleW inside Kernel32.dll.
+# - Windows terminal read those bytes and prints them onto monitor.
+#
+# ERROR MATRIX (Testing as i go)
+# ERROR 1:print("Welcome to Fantasy Quest!) ->
+# #SyntaxError: unterminated string literal.
+# Note: forgot to close text string with matching quote mark.
+#
+# ERROR 2:Print("Welcome to Fantasy Quest!") ->
+# NameError: name 'Print' is not defined.
+# Note: Python is case-sensitive and does not recognize capital functions.
+#
+# ERROE 3:print "Welcome to Fantasy Quest!" ->
+# SyntaxError: Missing parentheses in call.
+# Note: Python 3 requires parntheses around function inputs.
+#
+# THE GHOST ERROR print("Welcome to Fantasy Quest! ⚔️") ->
+# Triggers character drop.
+# On my Windows PowerShell screen, this displays as:
+# Welcome to Fantasy Quest! ??
+# 
+# Note: Windows consoles use old code pages that miss complex emojis.
+
